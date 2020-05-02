6ec15b42124b741c1f6151fcf5cfdc0c54925a7d
Daniel Lawrence <lawrence.danielt@gmail.com>
2020-05-02 15:36:23 -0400

    Add svelte frontend with calendar page

---
+++ web/App.svelte
@@ -0,0 +1,37 @@
+<script>
+  import FullCalendar from "./FullCalendar.svelte";
+  import { onMount } from "svelte";
+
+  const fcConfig = {
+    height: "auto",
+    minTime: "07:00:00",
+    allDaySlot: false
+  };
+
+  let calendarComponent;
+  let fc;
+
+  onMount(() => {
+    fc = calendarComponent.getCalendar();
+  });
+
+  function dateClick({ detail: event }) {
+    event = {
+      ...event,
+      editable: true,
+      startEditable: true,
+      durationEditable: true
+    };
+    fc.addEvent(event);
+  }
+
+  function eventClick({ detail: event }) {
+    event.event.remove();
+  }
+</script>
+
+<FullCalendar
+  config={fcConfig}
+  bind:this={calendarComponent}
+  on:dateClick={dateClick}
+  on:eventClick={eventClick} />
---
+++ web/FullCalendar.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { Calendar } from "@fullcalendar/core";
+  import timeGridPlugin from "@fullcalendar/timegrid";
+  import interaction from "@fullcalendar/interaction";
+  import { onMount, onDestroy, createEventDispatcher } from "svelte";
+
+  import "@fullcalendar/core/main.css";
+  import "@fullcalendar/timegrid/main.css";
+
+  const dispatch = createEventDispatcher();
+
+  let domCalendar;
+  let calendar;
+  export let config;
+
+  const event = [];
+
+  onMount(() => {
+    calendar = new Calendar(domCalendar, {
+      plugins: [timeGridPlugin, interaction],
+      dateClick: event => dispatch("dateClick", event),
+      eventClick: event => dispatch("eventClick", event),
+      eventDragStart: event => dispatch("eventDragStart", event),
+      eventDragStop: event => dispatch("eventDragStop", event),
+      eventDrop: event => dispatch("eventDrop", event),
+      eventLeave: event => dispatch("eventLeave", event),
+      eventMouseEnter: event => dispatch("eventMouseEnter", event),
+      eventMouseLeave: event => dispatch("eventMouseLeave", event),
+      eventResize: event => dispatch("eventResize", event),
+      eventResizeStart: event => dispatch("eventResizeStart", event),
+      eventResizeStop: event => dispatch("eventResizeStop", event),
+      ...config
+    });
+    calendar.render();
+  });
+
+  onDestroy(() => {
+    calendar.destroy();
+  });
+
+  export function getCalendar() {
+    return calendar;
+  }
+</script>
+
+<div bind:this={domCalendar} />
